b97e3425afbe5b62e7e607922dce14de1688103a
Clement Gautriaud <cgautriaud@orange.fr>
2024-03-22 14:43:05 +0100

    entities

--- src/main/java/com/doranco/examspring/model/entity/Author.java
+++ src/main/java/com/doranco/examspring/model/entity/Author.java
@@ -2,9 +2,45 @@ package com.doranco.examspring.model.entity;
 
 public class Author {
 
+	private int id;
+	private String name;
+	private String biography;
+	
     public Author() {}
 
     public Author(String name, String biography) {
-
+    	this.name = name;
+    	this.biography = biography;
     }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBiography() {
+		return biography;
+	}
+
+	public void setBiography(String biography) {
+		this.biography = biography;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", name=" + name + ", biography=" + biography + "]";
+	}
+    
+    
 }
--- src/main/java/com/doranco/examspring/model/entity/Borrowing.java
+++ src/main/java/com/doranco/examspring/model/entity/Borrowing.java
@@ -4,12 +4,59 @@ import java.time.LocalDate;
 
 public class Borrowing {
 
+	private int ind;
+	private int bookId;
+	private LocalDate borrowDate;
+	private LocalDate dueDate;
+	
     public Borrowing() {
 
     }
 
     public Borrowing(int bookId, LocalDate borrowDate, LocalDate dueDate) {
-
+    	this.bookId = bookId;
+    	this.borrowDate = borrowDate;
+    	this.dueDate = dueDate;
     }
+
+	public int getInd() {
+		return ind;
+	}
+
+	public void setInd(int ind) {
+		this.ind = ind;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public LocalDate getBorrowDate() {
+		return borrowDate;
+	}
+
+	public void setBorrowDate(LocalDate borrowDate) {
+		this.borrowDate = borrowDate;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Borrowing [ind=" + ind + ", bookId=" + bookId + ", borrowDate=" + borrowDate + ", dueDate=" + dueDate
+				+ "]";
+	}
+    
+    
 
 }
