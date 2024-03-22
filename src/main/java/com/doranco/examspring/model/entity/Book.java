206f3e866658afd27082dd534ef9900b293260fe
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-03-22 15:10:25 +0100

    Ajout de l'entité Book

--- src/main/java/com/doranco/examspring/model/entity/Book.java
+++ src/main/java/com/doranco/examspring/model/entity/Book.java
@@ -1,11 +1,75 @@
 package com.doranco.examspring.model.entity;
 
+import jakarta.persistence.*;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String publisher;
+    private int publicationYear;
+    private int pageCount;
 
     public Book() {}
 
     public Book(String title, String author, String publisher, int publicationYear, int pageCount) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.publicationYear = publicationYear;
+        this.pageCount = pageCount;
+    }
 
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
     }
 }
